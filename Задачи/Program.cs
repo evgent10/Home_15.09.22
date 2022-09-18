5dd2741234ffbfbf873d99808305a9af09b90df6
evgent10 <evgent10@ya.ru>
2022-09-18 13:49:23 +0300

    Задача 4

--- Задачи/Program.cs
+++ Задачи/Program.cs
@@ -19,3 +19,27 @@ if (b < max) min = b;
 
 Console.WriteLine($"{max} больше чем {min}, max = {max}");
 
+
+
+
+// ЗАДАЧА 4.
+// Напиши программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число: ");
+int a1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число: ");
+int b1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите третье число: ");
+int c1 = int.Parse(Console.ReadLine()!);
+
+int max1 = a;
+
+if (a1 > max1) max1 = a1;
+if (b1 > max1) max1 = b1;
+if (c1 > max1) max1 = c1;
+
+Console.Write($"Максимальное число {max1}");
+
