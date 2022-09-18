ee12d86c0dc43f78538b31f35f3ca54bbcf2d098
evgent10 <evgent10@ya.ru>
2022-09-18 13:42:14 +0300

    Задача 2

--- Задачи/Program.cs
+++ Задачи/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ЗАДАЧА 2.
+// Написать программу, которая на вход два числа и выдаёт,
+// какое число больше, а какое меньше.
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+int min = b;
+
+if (a < max) min = a;
+if (b < max) min = b;
+
+Console.WriteLine($"{max} больше чем {min}, max = {max}");
+
